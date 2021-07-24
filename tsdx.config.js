96abc355e7d1a54b3e32a3ce659976f3e3720734
unknown <pracharya2601@gmail.com>
2021-07-23 20:39:29 -0700

    style(tsdx.config.js): bundeling

--- tsdx.config.js
+++ tsdx.config.js
@@ -3,6 +3,9 @@ const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 module.exports = {
   rollup(config, options) {
+    if (config.output.format === 'umd') {
+      delete config.external;
+    }
     config.plugins.push(
       postcss({
         plugins: [
